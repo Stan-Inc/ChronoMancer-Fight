561b6a9440d745796e4c25cd91b05b2fce177250
Asterum <45477557+Asterum@users.noreply.github.com>
2018-12-13 19:36:55 -0700

    Add files via upload

---
+++ TurnManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class TurnManager : MonoBehaviour {
+    public const float t = .0166666666666666f;
+    public float PTT = 8f;
+    public bool PlayerTurn = true;
+    public float CTT = 0f;
+    public Image TurnCC;
+    public Image TurnPL;
+    public bool CutScene;
+    public void TurnSys()
+    
+    {
+        if (!CutScene) {
+        if (PlayerTurn && PTT > 0)
+        {
+            PTT -= t;
+            Debug.Log("MY TURN");
+            
+        }
+        else if (!PlayerTurn && CTT >0)
+        {
+            CTT -= t;
+            Debug.Log("CTURN");
+        }
+        else
+        {
+            Debug.Log("Turn Over!");
+            TurnHandOff();
+            //Insert Cutscene animations in TurnHandOff()
+        }
+        TurnPL.fillAmount = PTT / 8f;
+        TurnCC.fillAmount = CTT / 8f;
+        }
+    }
+    public void TurnHandOff()
+     {    if (PlayerTurn)
+        {
+            PlayerTurn = false;
+            //Insert Cutscene animation Here (reminder set CScene to True then to false)
+            CTT = 8;
+            PTT = 0;
+        }
+        else
+        {
+            PlayerTurn = true;
+            //Insert Cutscene animation Here (reminder set CScene to True then to false)
+            PTT = 8;
+            CTT = 0;
+        }
+    }
+}
