f42beef816fde7e22bfee00b70a1405acdfc0e0c
David Luo <45477557+Asterum@users.noreply.github.com>
2018-12-18 21:16:59 -0700

    Add files via upload

---
+++ Fight.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fight : MonoBehaviour {
+public TurnManager Turns;
+    public TRManager TR;
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetButton("CutScene"))
+            GetComponent<TurnManager>().CutScene = true;
+        //Used For Debuging, Remember to Remove
+        Turns.GetComponent<TurnManager>().TurnSys();
+
+        
+        if (GetComponent<TRManager>().TRBool || (GetComponent<TurnManager>().PlayerTurn && !GetComponent<TurnManager>().CutScene) )
+        {
+            PlayerControl();
+        }
+        GetComponent<TurnManager>().CutScene = false;
+        //remove this line after CutScenes are actualy implemented
+    }
+    void PlayerControl()
+        //Probobly only enter/select will be put here for final and all other input will be put in a different void Controlls here will be restricted durring enemy turns and CutScenes
+    {
+        if (Input.GetButton("Horizontal"))
+            Debug.Log("Menu HZ");
+        if (Input.GetButton("Vertical"))
+            Debug.Log("Menu VT");
+        if (Input.GetButton("Submit"))
+            Debug.Log("Menu ENT");
+        
+    } 
+   
+}
---
+++ TRManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TRManager : MonoBehaviour {
+    public float TR;
+    public bool TRBool;
+    public bool TRused;
+    public Image TRImage;
+    public const float t = .0166666666666666f;
+    // Pls Change this Constant to a more acurate 1/60 fraction
+    void Start () {
+        TR = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (TR > 8)
+        {
+            TR = 8;
+        }
+        if (Input.GetButton("TR") && TR > 0 && GetComponent<TurnManager>().PlayerTurn)
+        {
+            TR -= GetComponent<TurnManager>().DificultyCoef * t;
+            TRBool = true;
+            TRused = true;
+        }
+        else if (Input.GetButton("TR") && TR > 0 && !GetComponent<TurnManager>().PlayerTurn)
+        {
+            TR -= GetComponent<TurnManager>().DificultyCoef*1.5f *t;
+            TRBool = true;
+            TRused = true;
+        }
+        else
+        {  
+            TRBool = false;
+            
+        }
+        if (TR < 0)
+            TR = 0;
+        TRImage.fillAmount= TR/8f;
+	}
+}
